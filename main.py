6250b166665c65b7e048df7960d224ad442f3be7
saksham-42 <shakshamnigam.144@gmail.com>
2026-03-14 20:46:19 +0530

    feat : added CORS and custom error handling

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routers import users
-
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 app.include_router(users.router)
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins = ["*"],
+        allow_headers = ["*"],
+        allow_methods = ["*"]
+)
+
+@app.exception_handler(404)
+def not_found_error(request:Request, exc):
+    return JSONResponse(status_code=404,content ={"code": 404, "detail": "Resource not found"})
+
+@app.exception_handler(RequestValidationError)
+def validation_error_handler(request:Request, exc : RequestValidationError):
+    return JSONResponse(status_code= 422, content= {"code" : 422, "detail" : "Invalid request"})
 
 @app.get("/")
 def root():
