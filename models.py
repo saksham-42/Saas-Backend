d102e13bf77c22f920320c46fd89eb180563f069
saksham-42 <shakshamnigam.144@gmail.com>
2026-03-15 23:30:58 +0530

    feat : add User database model

---
+++ models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Integer, Column, String
+from db import Base
+class user(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer,primary_key=True, index=True)
+    name = Column(String, nullable= False)
+    email = Column(String, nullable= False, unique= True)
+    age = Column (Integer, nullable= False)
