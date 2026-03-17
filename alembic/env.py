ca294a437f4d5b870149a44488bdb3004599c469
saksham-42 <shakshamnigam.144@gmail.com>
2026-03-18 00:28:56 +0530

    fix : load DATABASE_URL from .env in Alembic env.py

--- alembic/env.py
+++ alembic/env.py
@@ -5,9 +5,15 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from dotenv import load_dotenv
+import os
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
