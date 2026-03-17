b6c9c8ec544409466dec3590db82aec043aa4bce
saksham-42 <shakshamnigam.144@gmail.com>
2026-03-17 23:49:45 +0530

    refractor: remove root models.py, moved to models/user.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Integer, Column, String
-from db import Base
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer,primary_key=True, index=True)
-    name = Column(String, nullable= False)
-    email = Column(String, nullable= False, unique= True)
-    age = Column (Integer, nullable= False)
