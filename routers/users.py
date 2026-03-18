e73b8b14a39149ddbd5718a0f1ff745332567ab6
saksham-42 <shakshamnigam.144@gmail.com>
2026-03-18 23:10:43 +0530

    feat: connected users router to local database, removed dictionary

--- routers/users.py
+++ routers/users.py
@@ -1,66 +1,43 @@
-from fastapi import FastAPI, HTTPException, APIRouter
+from fastapi import Depends, HTTPException, APIRouter
 from schemas import User_response, User_create, Update_user
-from typing import Optional
+from db import get_database
+from sqlalchemy.orm import Session
+import crud.users as crud
 
 router = APIRouter(
     prefix = "/users",
     tags = ["users"]
 )
 
-users = {
-    1 : {
-        "name" : "Ryan", 
-        "age" : "21", 
-        "email" : "ryan@gmail.com"
-    },
-    2 : {
-        "name" : "Marcel",
-        "age" : "25",
-        "email" : "mar_cel_1@gmail.com"
-    }
-}
+@router.get("/",response_model=list[User_response])
+def get_all_users(db:Session = Depends(get_database), skip: int = 0, limit: int = 10):
+    return crud.get_users(db, skip=skip, limit = limit)
 
-@router.get("/")
-def get_all_users(skip: int = 0, limit: int = 10):
-    user_list = list(users.values())
-    return user_list[skip : skip + limit]
-
-@router.get("/get-user/{user_id}")
-def get_user(user_id : int):
-    if user_id not in users:
+@router.get("/{user_id}", response_model=User_response)
+def get_user(user_id : int, db:Session = Depends(get_database)):
+    user = crud.get_user(db,user_id)
+    if not user:
         raise HTTPException(status_code = 404, detail="User doesn't exist")
-    return users[user_id]
+    return user
 
-@router.get("/search")
-def search_users(name : str):
-    results = {uid : u for uid, u in users.items()
-               if name.lower() in u["name"].lower()}
-    if not results:
-        raise HTTPException(status_code=404, detail= " No users found")
-    return results
-
-@router.post("/add-users/{user_id}")
-def add_user(user_id : int, user : User_create):
-    if user_id in users:
-        raise HTTPException(status_code=400, detail="User already exists")
-    users[user_id] = user
-    return users[user_id]
+@router.post("/", response_model= User_response)
+def add_user(user : User_create, db:Session = Depends(get_database)):
+    existing = crud.get_user_by_email(db, user.email)
+    if existing:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db,user)
     
-@router.put("/update-user/{user_id}")
-def update_users(user_id : int, user : Update_user):
-    if user_id not in users:
+@router.put("/{user_id}", response_model=User_response)
+def update_user(user_id : int, user : Update_user, db : Session = Depends(get_database)):
+    update_user = crud.get_user(db,user_id)
+    if not update_user:
         raise HTTPException(status_code= 404, detail="User doesn't exist")
-    if user.name is not None:
-        users[user_id].name = user.name
-    if user.age is not None:
-        users[user_id].age = user.age
-    if user.email is not None:
-        users[user_id].email = user.email
-    return users[user_id]
+    return crud.update_user(db, user_id, user)
 
-@router.delete("/delete-user/{user_id}")
-def delete_user(user_id : int):
-    if user_id not in users:
+@router.delete("/{user_id}")
+def delete_user(user_id : int,db: Session = Depends(get_database)):
+    db_user = crud.get_user(db, user_id)
+    if not db_user:
         raise HTTPException(status_code= 404, detail="The user doesn't exist")
-    del users[user_id]
-    return {"Success" : "User successfully deleted"}
+    crud.delete_user(db, user_id)
+    return {"Success" : "User successfully deleted!"}
