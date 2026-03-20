351881c1254a0de1ec983210322de9a875afb8b3
saksham-42 <shakshamnigam.144@gmail.com>
2026-03-20 23:21:26 +0530

    feat: added org_id FK and relationship to User model

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from db import Base
 
 class User(Base):
@@ -8,3 +9,6 @@ class User(Base):
     name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False, index=True)
     age = Column(Integer, nullable=False)
+    org_id = Column(Integer, ForeignKey("organizations.id"), nullable= True)
+
+    organization = relationship("Organization", back_populates="users")
