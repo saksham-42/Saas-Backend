dca7ab7f448a4e71f418e6f476f76448e095de41
saksham-42 <shakshamnigam.144@gmail.com>
2026-03-28 15:45:08 +0530

    feat: added refresh token CRUD operations

---
+++ crud/refresh_tokens.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+from models.refresh_token import RefreshToken
+from datetime import datetime, timedelta
+
+def save_refresh_token(db: Session, token: str, user_id: int):
+    expires_at = datetime.utcnow()+timedelta(days=7)
+    db_token = RefreshToken(token=token, user_id=user_id, expires_at= expires_at)
+    db.add(db_token)
+    db.commit()
+    db.refresh(db_token)
+    return db_token
+
+def get_refresh_token(db: Session, token: str):
+    return db.query(RefreshToken).filter(RefreshToken.token == token).first()
+
+def revoke_refresh_token(db: Session, token: str):
+    db_token = get_refresh_token(db, token)
+    if db_token:
+        db_token.revoked = True
+        db.commit()
+
