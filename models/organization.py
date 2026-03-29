c1b9c82de835babf04383bcf3b90c75aae1864cd
saksham-42 <shakshamnigam.144@gmail.com>
2026-03-29 22:41:31 +0530

    feat: added slug, owner_id, created_at to Organization model

--- models/organization.py
+++ models/organization.py
@@ -1,12 +1,16 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from db import Base
+from datetime import datetime
 
 class Organization(Base):
     __tablename__ = "organizations"
 
     id = Column(Integer, index= True, primary_key=True)
-    name = Column(String, unique= True, nullable=False) 
+    name = Column(String, unique= True, nullable=False)
+    slug = Column(String, unique= True, nullable=False, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"),nullable=False)
+    created_at = Column(DateTime, nullable= False, default=datetime.utcnow)
 
     users = relationship("User", back_populates="organization")
 
