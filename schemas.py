bb42b1578dc2229d2ac6b782b571ebc6647fd17e
saksham-42 <shakshamnigam.144@gmail.com>
2026-03-30 17:05:27 +0530

    feat: added Org_create and Org_response schemas

--- schemas.py
+++ schemas.py
@@ -13,6 +13,9 @@ class Update_user(BaseModel):
     name : Optional[str] = Field(default=None, min_length=2, max_length=50)
     age : Optional[int] = Field(default=None, gt=15, lt=75)
     email : Optional[EmailStr] = None
+    org_id : Optional[int] = None
+    password : Optional[str] = Field(default=None, min_length=6)
+    role : Optional[str] = Field(default=None, min_length=5)
 
 class User_response(BaseModel):
     id : int
@@ -27,3 +30,18 @@ class User_response(BaseModel):
 class Login(BaseModel):
     email: EmailStr
     password: str
+
+
+class Org_create(BaseModel):
+    name : str = Field(min_length=2, max_length=50)
+    slug : str = Field(min_length=2, max_length=50)
+
+
+class Org_response(BaseModel):
+    id: int
+    name: str
+    slug: str
+    owner_id: int
+
+    class Config:
+        from_attributes = True
