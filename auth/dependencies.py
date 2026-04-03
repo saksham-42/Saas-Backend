afbb8b1eef504595def92748e4b3da178978df8b
saksham-42 <shakshamnigam.144@gmail.com>
2026-04-03 22:53:17 +0530

    feat: added get_org_member dependency

--- auth/dependencies.py
+++ auth/dependencies.py
@@ -7,6 +7,7 @@ from fastapi.security import OAuth2PasswordBearer
 import os
 from dotenv import load_dotenv
 from models.user import User
+from models.organization_member import OrganizationMember
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 
@@ -33,3 +34,9 @@ def require_admin(current_user: User=Depends(get_user)):
     if current_user.role != "admin":
         raise HTTPException(status_code=403, detail="Admins Only")
     return current_user
+
+def get_org_member(org_id: int, curr_user: User=Depends(get_user), db: Session=Depends(get_database)):
+    member = db.query(OrganizationMember).filter(OrganizationMember.org_id==org_id, OrganizationMember.user_id==curr_user.id).first()
+    if not member:
+        raise HTTPException(status_code=403, detail="Not a member of this organization")
+    return member
