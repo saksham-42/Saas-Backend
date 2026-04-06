d84bf1698ed43942f984565ea04db946b89a8251
saksham-42 <shakshamnigam.144@gmail.com>
2026-04-06 16:45:54 +0530

    feat: add TaskPriority and due_date

--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, field_validator
 from typing import Optional
-from datetime import datetime
+from datetime import datetime, timezone
 from enum import Enum
 
 class User_create(BaseModel):
@@ -48,32 +48,47 @@ class Org_response(BaseModel):
     class Config:
         from_attributes = True
 
+class TaskStatus(str, Enum):
+    pending = "pending"
+    in_progress = "in_progress"
+    completed = "completed"
+
+class TaskPriority(str, Enum):
+    low = "low"
+    medium = "medium"
+    high = "high"
+    urgent = "urgent"
 
 class Task_create(BaseModel):
     title : str = Field(min_length=2, max_length=50)
     description : Optional[str] = None
-    status : str = Field(default="pending")
+    status : TaskStatus = TaskStatus.pending
+    priority : TaskPriority = TaskPriority.medium
     assigned_to : Optional[int] = None 
+    due_date : Optional[datetime]
+    
+    @field_validator("due_date")
+    @classmethod
+    def due_date_must_be_future(cls, v):
+        if v and v < datetime.now(timezone.utc):
+            raise ValueError("due_date can't be in past")
+        return v
 
 class Task_response(BaseModel):
     id : int
     description : Optional[str]
     title : str
     status : str
+    priority : str
     created_at : datetime
     is_deleted : bool
     org_id : int
     assigned_to : Optional[int]
+    due_date : Optional[datetime]
 
     class Config:
         from_attributes = True
         
-
-class TaskStatus(str, Enum):
-    pending = "pending"
-    in_progress = "in_progress"
-    completed = "Completed"
-
 class TaskUpdate(BaseModel):
     status : TaskStatus
 
