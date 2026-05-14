6e68ce3476f29c7e74bc1df2148c50a5803e3c6f
saksham-42 <shakshamnigam.144@gmail.com>
2026-05-14 20:11:20 +0530

    fix: disable limiting in test environment

--- app/core/limiter.py
+++ app/core/limiter.py
@@ -1,4 +1,11 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
+import os
+
+def get_limit(limit:str):
+    "Return limit in production, but bypass in testing"
+    if os.getenv("TESTING") == "true":
+        return "10000/minute"
+    return limit
 
 limiter = Limiter(key_func=get_remote_address)
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from app.core.db import get_database
 from app.schemas.user import User_create, User_response, Login
 import app.services.auth_service as auth_service
-from app.core.limiter import limiter
+from app.core.limiter import limiter, get_limit
 import logging
 
 logger = logging.getLogger(__name__)
@@ -18,14 +18,14 @@ def send_welcome_email(email: str):
     logger.info("Welcome email sent to %s", email)
 
 @router.post("/register", response_model=User_response)
-@limiter.limit("3/hour")
+@limiter.limit(get_limit("3/hour"))
 def register(request: Request, user: User_create, background_tasks: BackgroundTasks , db: Session = Depends(get_database)):
     result = auth_service.register(user, db)
     background_tasks.add_task(send_welcome_email,user.email)
     return result
 
 @router.post("/login")
-@limiter.limit("5/minute")
+@limiter.limit(get_limit("5/minute"))
 def login(request: Request, user: Login, db: Session = Depends(get_database)):
     return auth_service.login(user, db)
 
